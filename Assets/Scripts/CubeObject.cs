3be412d1e72c6184d15edce29b70c3752a2e5f47
Ian Miller <indmillr@gmail.com>
2023-04-06 14:42:10 -0500

    Cube Object
    Inheritance, Abstraction

---
+++ Assets/Scripts/CubeObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Inheritance
+public class CubeObject : Shape
+{
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        Move(); // Abstraction
+    }
+}
