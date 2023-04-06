8f3a80a4c8103b06be2d8342a5d024d7a637f9c3
Ian Miller <indmillr@gmail.com>
2023-04-06 15:51:30 -0500

    Update SphereObject.cs
    modifications to Speed variable

--- Assets/Scripts/SphereObject.cs
+++ Assets/Scripts/SphereObject.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+<<<<<<< Updated upstream
 using System.Diagnostics;
 using UnityEngine;
 
 public class SphereObject : Shape // Inheritance
+=======
+using UnityEngine;
+
+// Inheritance
+public class SphereObject : Shape
+>>>>>>> Stashed changes
 {
     private float speedMultiplier = 2.0f;
 
     private void Start()
     {
+<<<<<<< Updated upstream
         Speed += speedMultiplier;
         Debug.Log($"{Speed}");
         Speed -= speedMultiplier;
         Speed -= speedMultiplier;
         Debug.Log($"{Speed}");
+=======
+        speed += speedMultiplier;
+        Debug.Log($"{speed}");
+        speed -= speedMultiplier;
+        speed -= speedMultiplier;
+        Debug.Log($"{speed}");
+>>>>>>> Stashed changes
     }
 
     public override void Move()
@@ -23,8 +38,14 @@ public class SphereObject : Shape // Inheritance
         transform.Translate(Vector3.forward * Time.deltaTime);
     }
 
+<<<<<<< Updated upstream
     void Update()
     {
         Move(); // Abstraction
+=======
+    private void Update()
+    {
+        Move();
+>>>>>>> Stashed changes
     }
 }
